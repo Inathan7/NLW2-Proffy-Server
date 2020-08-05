d2f9fb018632148a5f612053e25ba105d8510d9c
unknown <inathan.moreira.10@gmail.com>
2020-08-05 19:48:52 -0300

    Day 2 #NLW2

---
+++ src/database/migrations/01_create_classes.ts
@@ -0,0 +1,13 @@
+import Knex from 'Knex';
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable('classes', table => {
+        table.increments('id').primary();
+        table.string('subject').notNullable();
+        table.decimal('cost').notNullable();
+    });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('classes');
+}
